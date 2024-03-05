e76bc63823bce3264e952e558e724d9b1221b77f
NithinU2802 <nithinu2810@gmail.com>
2024-03-05 23:35:45 +0530

    Route products

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express=require('express')
 const app=express()
 
+const productRoute=require("./api/routes/products")
+
+app.use('/products',productRoute);
+
 app.use((req,res,next)=>{
     res.status(200).json({
         message: "NO ERROR Nithin...!"

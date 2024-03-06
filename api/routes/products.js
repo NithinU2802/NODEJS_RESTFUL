df974151f0a7b426b3f7a6ecc6dbf036f667cc13
NithinU2802 <nithinu2810@gmail.com>
2024-03-06 15:56:33 +0530

    Orders Update

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,15 +2,41 @@ const express= require("express");
 const router=express.Router();
 
 router.get('/',(req,res,next)=>{
-    res.status(100).json({
+    res.status(200).json({
         message:'I am Get Request of products'
     })
 })
 
 
 router.post('/',(req,res,next)=>{
-    res.status(100).json({
+    res.status(200).json({
         message:'I am Post Request of products'
+    })
+})
+
+router.get('/:productId',(req,res,next)=>{
+    const id=req.params.productId;
+    if(id=='special'){
+        res.status(200).json({
+            message: 'Your id is special',
+            id: id
+        })
+    }else{
+        res.status(200).json({
+            message: 'You passed an ID'
+        })
+    }
+})
+
+router.patch('/:productId',(req,res,next)=>{
+    res.status(200).json({
+        message: 'Updated Product..!'
+    })
+})
+
+router.delete('/:productId',(req,res,next)=>{
+    res.status(200).json({
+        message: 'Deleted Product..!'
     })
 })
 
--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express=require('express')
 const app=express()
 
 const productRoute=require("./api/routes/products")
+const orderRoute=require("./api/routes/orders")
 
 app.use('/products',productRoute);
+app.use('/orders',orderRoute)
 
-app.use((req,res,next)=>{
-    res.status(200).json({
-        message: "NO ERROR Nithin...!"
-    })
-})
+// app.use((req,res,next)=>{
+//     res.status(200).json({
+//         message: "NO ERROR Nithin...!"
+//     })
+// })
 
 module.exports=app;
